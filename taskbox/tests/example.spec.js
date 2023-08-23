ecf983e6349726c06a444e88982c0d75f1784234
Connor Vidlock <connor.vidlock@gmail.com>
2023-08-23 17:13:32 -0500

    Revert "Merge branch 'main' into notick-work"
    This reverts commit 3d90ddcae3a7fb88a44786c9ff092ed98f3e667b, reversing
    changes made to 21e1e1ab79ae69b15392efd52d17b925c435f77e.

--- taskbox/tests/example.spec.js
+++ taskbox/tests/example.spec.js
@@ -1,15 +1,11 @@
 // @ts-check
-import { test, expect, takeArchive } from '@chromaui/test-archiver';
+import { test, expect } from '@chromaui/test-archiver';
 
-test('has title', async ({ page }, testInfo) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-  await takeArchive(page, testInfo);
-
-  await page.goto('https://www.google.com');
-  await takeArchive(page, testInfo);
 });
 
 test('get started link', async ({ page }) => {
